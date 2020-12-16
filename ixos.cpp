8bfb6ba071313aa2a0803946d100d3e4e4aaa31c
c611701142 <km9216bp@gmail.com>
2020-12-16 22:29:38 +0900

    Update ixos.cpp

--- ixos.cpp
+++ ixos.cpp
@@ -10,7 +10,7 @@
 static constexpr uint64_t default_size = std::pow(2,24);//(ここは24)
 std::vector<int> B_ = {13,-7,5};//ビットシフトパターン(32ビット)
 std::vector<int> B_1 = {5,-7,13};//ビットシフトパターン(32ビット)
-int k = (std::log(default_size)/std::log(2)) + 7;//mask値の決定のため、P, C 拡張時にインクリメント
+int k = (std::log(default_size)/std::log(2)) + 8;//mask値の決定のため、P, C 拡張時にインクリメント
 
 uint64_t xos(uint64_t x){//前&x
     uint64_t maskXos_ = (1 << k);
@@ -45,7 +45,6 @@ int loop_time(int k,int b){
 
 uint64_t ixos(uint64_t x){//前シード値から出力
     uint64_t maskXos_ = (1 << k);
-	uint64_t z = 0;
 	int n = 0;
 	//xorの回数は2^ceil(log2(k/b))
 	for(int b: B_1){
@@ -56,6 +55,7 @@ uint64_t ixos(uint64_t x){//前シード値から出力
 		else if(b < 0){
 			n = loop_time(k,-b);
 		}*/
+		//ビット列
 		if(b >= 0){
 			n = bit_len(k/b);
 		}
