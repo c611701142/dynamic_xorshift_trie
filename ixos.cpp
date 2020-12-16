6b1b2fae7b1fd391d2c3972908adbb8d4623f6e8
c611701142 <km9216bp@gmail.com>
2020-12-16 23:45:17 +0900

    Add files via upload

--- ixos.cpp
+++ ixos.cpp
@@ -13,7 +13,7 @@ std::vector<int> B_1 = {5,-7,13};//ビットシフトパターン(32ビット)
 int k = (std::log(default_size)/std::log(2)) + 8;//mask値の決定のため、P, C 拡張時にインクリメント
 
 uint64_t xos(uint64_t x){//前&x
-    uint64_t maskXos_ = (1 << k);
+    uint64_t maskXos_ = 1ull << k;
 	for(int b: B_){//ビットシフトパターン{a,b}なら先にa次にbの２回ループ
         //std::cout << "shift patern , seed" << b << "," << x << "\n";
 		if(b >= 0){
@@ -31,21 +31,18 @@ uint64_t xos(uint64_t x){//前&x
 int bit_len(uint32_t x) {
     assert(x > 0);
     int n = 0;
-    while ((1<<n) <= x-1)
+    while ((1ull<<n) <= x-1)
         ++n;
-    return 1 << n;
+    return n;
 }
 
 int loop_time(int k,int b){
-	double n1 = (std::log(k) - std::log(b))/std::log(2);
-	double n = std::ceil(n1);
-	int a = std::pow(2,n); 
-	return a;
+	assert(k > 0 and b > 0);
+    return 1 << bit_len((k-1) / b + 1);
 }
 
 uint64_t ixos(uint64_t x){//前シード値から出力
-    uint64_t maskXos_ = (1 << k);
-	int n = 0;
+    uint64_t maskXos_ = 1ull << k;
 	//xorの回数は2^ceil(log2(k/b))
 	for(int b: B_1){
 		/*	パターン１　そのまま
@@ -55,14 +52,7 @@ uint64_t ixos(uint64_t x){//前シード値から出力
 		else if(b < 0){
 			n = loop_time(k,-b);
 		}*/
-		//ビット列
-		if(b >= 0){
-			n = bit_len(k/b);
-		}
-		else if(b < 0){
-			n = bit_len(k/-b);
-		}
-
+		int n = loop_time(k, abs(b)) - 1;
 		std::cout << "roop time" << n << "\n";
 		std::cout << "ixos b" << b << "\n";
 		for(int i = 0;i < n;i++){
@@ -79,7 +69,7 @@ uint64_t ixos(uint64_t x){//前シード値から出力
 } 
 
 int main(){
-    uint64_t seed = 98;
+    uint64_t seed = 47;
     uint64_t x = xos(seed);//シード値→出力値
 	std::cout << "-----------------------"<< "\n";
     uint64_t s1 = ixos(x);//出力値→シード値
