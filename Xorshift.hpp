6d4702adda0b64b9eae791480b9ed777fac75b22
c611701142 <km9216bp@gmail.com>
2020-12-25 09:27:53 +0900

    Add files via upload

--- Xorshift.hpp
+++ Xorshift.hpp
@@ -20,177 +20,236 @@ public:
 static constexpr int null = -1;//データが入っていないことを示す値
 static constexpr int invalid = -1;
 static constexpr int invalid_key = -1;
-static constexpr uint64_t default_size = std::pow(2,24);
+static constexpr uint64_t default_size = std::pow(2,4);
+static constexpr uint8_t MaxUint8_t = 0xFF;//255　文字コード最大
 Xorshift(){
 	pc_.resize(default_size);
 	exists.resize(default_size,false);
+    exists[0] = true;//0番目は使わない
 }
 
 private:
 std::vector<int> B_ = {13,-7,5};//ビットシフトパターン
+std::vector<int> B_1 = {5,-7,13};//ビットシフトパターン(32ビット)
 int hash_use = 0;//配列P,C の使用数
 int k = (std::log(default_size)/std::log(2)) + 8;//mask値の決定のため、P, C 拡張時にインクリメント
 //対数でkを決める()
 
+public:
 struct DataItem {
     int p,c;
     DataItem(): p(invalid),c(invalid){}
 };
 std::vector<DataItem> pc_ ;//P,C配列
-public:
 std::vector<bool> exists;//空判定配列
 
 private:
+int get_seed(int t)const{//配列番号、パリティ値、衝突回数からシード値を得る
+    uint64_t x = 0;
+    if(pc_[t].c == 0){
+        x = (t << 8) + pc_[t].p;
+    }
+    else{
+        x = (t << 8) + pc_[t].p;
+        for(int i = 0;i < pc_[t].c;i++){//衝突回数回ixosに代入することで、元のシード値をえる
+            x = ixos(x);//仮の出力値を得る(再xosする前の)
+        }
+    }
+    uint64_t seed = ixos(x);
+    return seed;
+}//ここから、親と遷移文字が分かる
+
 void expand_resize(){
-    display();
+    std::cout << "before table" << pc_.size() << "  " << k << std::endl;
+    //display();
 	//出力値を遷移先候補とパリティ値から復元する
     std::vector<DataItem> pc_2(2*pc_.size());//P,C配列
     std::vector<bool> exists2(2*pc_.size());//空判定配列
-    k++;//マスク更新
-    uint64_t seed = 0;
-    for(int i = 0;i < pc_.size();i++){
+    exists2[0] = true;//0番目は使わない
+    int parent = 0;uint8_t c = 0;
+    int replace = hash_use;
+    for(int i = 1;i < pc_.size();i++){
+        std::vector<uint8_t> codes;
         if(exists[i]){//使用要素
-            uint64_t t = i << 8;
-            uint64_t t1 = t +  pc_[i].p;//出力値を復元
-            seed = ixos(t1);//シード値を求める
-            t1 = xos(seed);//出力値更新
-            //親から再配置,未実装
-            pc_2[i].p = pc_[i].p;
-            pc_2[i].c = pc_[i].c;
-            exists2[i] = true;
-            std::cout << "seed = " << seed << "\n" ;
-            std::cout << "x[i] = " << t1 << "\n" ;
+            int s = get_parent(i);
+            c = get_charcode(i);
+            k++;
+            int new_t = 0;
+            if(s == parent){//set
+                uint64_t x1 = xos((s << 8) + c);//新しい出力値
+                new_t = x1 >> 8;//新しい遷移先
+                int collision = 0;
+                while(exists2[new_t]){//使用済みならば再Xos
+                    x1 = xos(x1);//出力値をxorに再代入
+		            new_t = x1 >> 8;
+                    collision++;   
+                }
+            int parity = x1 % 256;
+            pc_2[new_t].p = parity;
+            pc_2[new_t].c = collision;
+            exists2[new_t] = true;
+            parent = i;//10,13を親として、14,3,10番目の配列にアクセスしたい
+            i = 1;//もう一度右から探索
+            replace--;
+            }//親から再配置(まずは0からの遷移)
+            k--;//get_parentsでのxos,ixosのため、マスクをもとにもどす
         }
-        else{}
+        if(i == pc_.size()-1 && replace != 0){//左端まで行ってまだ残ってたら
+                exists[parent] = false; 
+                i = 1;
+                parent = 0;
+        }
+        //std::cout << replace << "\n";
+        if(replace == 0){
+            break;
+        }
     }
+    k++;//最終的に、マスク＋１に更新する
     pc_ = std::move(pc_2);
     exists = std::move(exists2);
+    //display();
 } 
-int node_count = 0;
+int get_parent(int t){//子の状態番号→出力値→シード値→親番号
+    uint64_t seed = get_seed(t);
+    return seed >> 8;
+}
+uint8_t get_charcode(int t){//子の状態番号→出力値→シード値→文字コード
+    uint64_t seed = get_seed(t);
+    return seed % 256;
+}
+
+
 public:
 void display(){
-    for(int i = 0; i < pc_.size();i++){
+    int collision_max = 0;
+    for(int i = 1; i < pc_.size();i++){
         //使用要素のみ表示
         if(exists[i]){
-        node_count++;
+            if(collision_max < pc_[i].c){
+                collision_max = pc_[i].c;
+            }
         std::cout << i << "    " << exists[i] << "       ";
         std::cout << pc_[i].p << "  |  " << pc_[i].c << std::endl;
         //配列番号 
         }
     }
-    //std::cout << "node_sum" << node_count << std::endl;
+    std::cout << "collision_max" << collision_max << std::endl;
 }
 
 //get関数ですが、今はそのまま返しているだけです
-int get_parity(uint64_t x)const{
+int get_parity(uint64_t x)const{//引数シード値
     uint64_t x1 = xos(x);
     int t = x1 >> 8;//遷移先
+    int parity = x1 % 256;
+    while(exists[t]){//使用済みならば再Xos
+        if(pc_[t].p == parity){
+            return pc_[t].p;
+        }
+        x1 = xos(x1);//
+        t = x1 >> 8;//遷移先
+        parity = x1 % 256;
+    }
 	return pc_[t].p;
 }
 
-int get_collision(uint64_t x)const{
+int get_collision(uint64_t x)const{//引数シード値
     uint64_t x1 = xos(x);
     int t = x1 >> 8;//遷移先
-	return  pc_[t].c;
+    int parity = x1 % 256;
+    while(exists[t]){//使用済みならば再Xos
+        if(pc_[t].p == parity){
+            return pc_[t].c;
+        }
+        x1 = xos(x1);//
+        t = x1 >> 8;//遷移先
+        parity = x1 % 256;
+    }
+	return pc_[t].c;
 }
-int get_nextnode(uint64_t x)const{
+int get_nextnode(uint64_t x)const{//引数シード値
 	uint64_t x1 = xos(x);
     int t = x1 >> 8;//遷移先
+    int parity = x1 % 256;
+    while(exists[t]){//使用済みならば再Xos
+        if(pc_[t].p == parity){
+            return t;
+        }
+        x1 = xos(x1);//
+        t = x1 >> 8;//遷移先
+        parity = x1 % 256;
+    }
 	return  t;
 }
 
+private:
 uint64_t xos(uint64_t x)const{//前&x
-    uint64_t maskXos_ = (1 << k);
+    uint64_t maskXos_ = 1ull << k;
 	for(int b: B_){//ビットシフトパターン{a,b}なら先にa次にbの２回ループ
         //std::cout << "shift patern , seed" << b << "," << x << "\n";
 		if(b >= 0){
-			x = (x ^ (x << b) % maskXos_);
+			x = (x ^ (x << b)) % maskXos_;
 		}
 		else if(b < 0){
-			x = (x ^ (x >> -b) % maskXos_);
+			x = (x ^ (x >> -b)) % maskXos_;
         }
 	}
 	return x;
 }
 
-uint64_t ixos(uint64_t x){//前&x
-    uint64_t maskXos_ = (1 << k);
-	uint64_t z = 0;
-	for(int b: B_){//ビットシフトパターン{a,b}なら先にa次にbの２回ループ
-		//if(k % b == 0){
+int bit_len(uint32_t x)const{
+    assert(x > 0);
+    int n = 0;
+    while ((1ull<<n) <= x-1)
+        ++n;
+    return n;
+}
+
+int loop_time(int k,int b)const{
+	assert(k > 0 and b > 0);
+    return 1 << bit_len((k-1) / b + 1);
+}
+
+uint64_t ixos(uint64_t x)const{//前シード値から出力
+    uint64_t maskXos_ = 1ull << k;
+	//xorの回数は2^ceil(log2(k/b))
+	for(int b: B_1){
+		int n = loop_time(k, abs(b)) - 1;
+		for(int i = 0;i < n;i++){
 			if(b >= 0){
-				for(int i = 0;i < (k/b)-1; i++){
-					z = (x ^ (x << b)) % maskXos_;
-				}
-				x = (x ^ (z << b)) % maskXos_;	
+				x = (x ^ (x << b)) % maskXos_;
 			}
 			else if(b < 0){
-				for(int i = 0;i < (k/-b)-1; i++){
-					z = (x ^ (x >> -b)) % maskXos_;
-				}
-				x = (x ^ (z << -b)) % maskXos_;
+				x = (x ^ (x >> -b)) % maskXos_;
 			}
-		//}
-		/*else{
-			if(b >= 0){
-				for(int i = 0;i < (k/b) + 1; i++){
-					x = (x ^ (x << b)) % maskXos_;
-				}		
-			}
-			else if(b < 0){
-				for(int i = 0;i < (k/-b)-1; i++){
-					x = (x ^ (x >> -b)) % maskXos_;
-				}
-			}
-		}*/
+		}
 	}
 	return x;
 } 
 
-
+public:
 //配列P,Cに要素を格納、衝突が起これば再配置
-void set(uint64_t x){//引数 : シード値
+void set(uint64_t seed){//引数 : シード値
 	int load_factor = hash_use*100/pc_.size();
-    //std::cout << "before_expand" << load_factor << "%" << std::endl;
-
     //keyによる探索の期待計算量が、負荷率をqとしてO(1/(1-q))になる
     if(load_factor >= 50){
         expand_resize();
         int load_factor2 = hash_use*100/pc_.size();
         std::cout << "after_expand" << load_factor2 << "%" << std::endl;
     }
-	uint64_t maskXos_ = (1 << k) -1;
-    uint64_t x1 = xos(x);
-    int t = x1 >> 8;//遷移先候補 8桁目以降
+    uint64_t x1 = xos(seed);
+    int t = x1 >> 8;//遷移先候補 8桁目以降  koko12
     int collision = 0;
-	//std::cout << "2の" << k << "\n";
-	//std::cout << "SIZE" << pc_.size() << "\n";
     while(exists[t]){//使用済みならば再Xos
         x1 = xos(x1);//出力値をxorに再代入
 		t = x1 >> 8;
         collision++;        
-        //std::cout << "re_xos" << "\n";
-		//std::cout << t << "\n";
-		//std::cout << exists[t] << "\n";
-        //std::cout << "c : " << collision << std::endl;
     }
     int parity = x1 % 256;
     pc_[t].p = parity;
     pc_[t].c = collision;
     exists[t] = true;
 	hash_use++;
-    std::cout << "output : " << x1 << std::endl;
-    //std::cout << "t : " << t << std::endl;
-    //std::cout << "p : " << parity << std::endl;
-    //std::cout << "c : " << collision << std::endl;
-    //std::cout << "insert- data"<< default_size << std::endl;                  
-	//std::cout << "SIZE = "<< pc_.size() << "  2^" << k <<  std::endl;
-    //std::cout << "SIZE : " << pc_.size() << std::endl;
-    //std::cout << "k : " << k << std::endl;
-    //std::cout << "defalt : " << default_size << std::endl;
-	}
-
-
+}
 
 };
 
--- Xorshift_trie.hpp
+++ Xorshift_trie.hpp
@@ -20,49 +20,33 @@ int create_seed(int node, uint8_t c)const{
     return (node << 8) + c;
 }
 
-int common_word = 0; 
+int node_count = 0; 
 public:
 void xor_try(const std::string& str) {
     int node = 0;
     int collision = 0;
     int parity = 0;
-    std::cout << "-----key_word-------" << str << std::endl;
+    //std::cout << "-----key_word-------" << str << std::endl;
     for (uint8_t c : str) {
-        //std::cout << "-----trans_word----" << c << std::endl;
-        uint64_t x = create_seed(node,c);//シード値
-        uint64_t x1 = shift.xos(x);
-        parity = shift.get_parity(x);
-        collision = shift.get_collision(x);
-        int t = shift.get_nextnode(x);
-        int parity2 = x1 % 256;//本来のパリティ値
-        //std::cout << "-----try start----" << std::endl;
-        //std::cout << "x : " << x1 << std::endl;
-        std::cout << "seed : " << x << std::endl;
-        //std::cout << "P : " << parity << std::endl;
-        //std::cout << "c: " << collision << std::endl;
-        //std::cout << "t: " << t << std::endl;
-        std::cout << "s : t " << node << " --> " << t << std::endl;
-        //std::cout << "-----try end----" << std::endl;
-        //接頭辞共有
-        //使用要素かつパリティ値が一致 (遷移先候補のみがかぶる可能性があるので)
-        //if(parity != -1 && collision != -1){
-        if(shift.exists[t] && parity == parity2){    
+        int t = shift.get_nextnode(create_seed(node,c));
+        if(shift.exists[t]){    
             node = t;
-            std::cout << "--common--" << "\n";
-            common_word++;
+            //std::cout << "--common--" << "\n";
         }
         else{
-            std::cout << "---new_node---" << std::endl;
-            shift.set(x);
-            node = t;
+           // std::cout << "---new_node---" << std::endl;
+            shift.set(create_seed(node,c));
+            //std::cout << "new s : new t " << node << " --> " << t << std::endl;
+            node = shift.get_nextnode(create_seed(node,c));//衝突回数の分修正する
+            node_count++;
         }
     }
     //終端文字格納
-    uint64_t x = (node << 8) + kLeafChar;
-    shift.set(x);
-    //std::cout << "---insert_#---" << std::endl;
-    //std::cout << common_word << std::endl;
-    //shift.display();
+    shift.set(create_seed(node,kLeafChar));
+    node_count++;
+    int t = shift.get_nextnode(create_seed(node,kLeafChar));
+    shift.display();
+    std::cout << "node : " << node_count << std::endl;
 }
 
 
